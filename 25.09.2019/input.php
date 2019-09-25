2360fdd0d2eac54374061759bda910a1c40caf60
kristjan.mikk <kristjan.mikk@khk.ee>
2019-09-25 11:33:07 +0300

    kasutaja sisend phps

--- 25.09.2019/input.php
+++ 25.09.2019/input.php
@@ -3,5 +3,23 @@
         <label for="nimi">Nimi:</label>
         <input type="text" id="nimi" name="nimi">
     </div>
+    <div>
+        <label for="perenimi">perenimi:</label>
+        <input type="text" id="perenimi" name="perenimi">
+    </div>
+    <div>
+        <label for="email">email:</label>
+        <input type="text" id="email" name="email">
+    </div>
+    <div>
+        <label for="pikkus">pikkus:</label>
+        <input type="number" id="pikkus" name="pikkus">
+    </div>
+    <div>
+        <label for="kaal">kaal:</label>
+        <input type="number" id="kaal" name="kaal">
+    </div>
+
+
     <input type="submit" value="saada">
 </form>
--- 25.09.2019/input_do.php
+++ 25.09.2019/input_do.php
@@ -1,6 +1,20 @@
 <?php
 // muutuja defineerimine ja väärtuse vütmine vormist
 $nimi = $_GET["nimi"];
+$perenimi = $_GET["perenimi"];
+$email = $_GET["email"];
+$pikkus = $_GET["pikkus"];
+$kaal = $_GET["kaal"];
+
+
+
 print "nimi:".$nimi."<br>";
+print "perenimi:".$perenimi."<br>";
+print "email:".$email."<br>";
+print "pikkus:".$pikkus."<br>";
+print "kaal:".$kaal."<br>";
+
+
+
 echo "<hr>";
 print '<a href="input.php"> sisesta andmed uuesti</a>';
