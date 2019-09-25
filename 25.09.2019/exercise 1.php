78cc92fe023e02ddd59e87f76f2b533f4f63f247
kristjan.mikk <kristjan.mikk@khk.ee>
2019-09-25 10:43:37 +0300

    teksti väljastus phps koos html elementidega

---
+++ 25.09.2019/exercise 1.php
@@ -0,0 +1,7 @@
+<?php
+
+// eesnimi, perenimi, vanus, email, pikkus:n.Nr, kaal (kg):NR
+
+
+print "<h1>Info minust</h1>"
+Print "Eesnimi: Kristjan <br> perenimi:Mikk <br> vanus: 19 <br> email: <a href='kristjan.mikk@khk.ee>'>kristjan.mikk@khk.ee</a> <br> pikkus: 180cm <br> kaal: 95kg";
