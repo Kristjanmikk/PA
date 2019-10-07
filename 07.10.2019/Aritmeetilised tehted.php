e03ccd02f62c959c3dd9aefa7cc617eae24e0dc6
kristjan.mikk <kristjan.mikk@khk.ee>
2019-10-07 14:56:16 +0300

    aritmeetilised tehted fahrenheit, celsius

--- 07.10.2019/Aritmeetilised tehted.php
+++ 07.10.2019/Aritmeetilised tehted.php
@@ -1,6 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aritmeetika</title>
+    <style>
+        table{
+            border: solid 2px #333;
+            border-collapse: collapse;
+        }
+        th, td{
+            text-align: center;
+            padding: 20px;
+        }
+        thead{
+            background-color: coral;
+        }
+        body{
+            font-family: "Agency FB";
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+
+
+
 <?php
 
-$x = 8:
+$x = 8;
 $y = 2;
 $liitmine = $x + $y;
 $lahutamine = $x - $y;
@@ -8,11 +38,68 @@ $korrutamine = $x * $y;
 $jagamine = $x / $y;
 $jaak = $x % $y;
 
-print"
-    <table></table>
+echo"
+    <table>
+        <thead>
+        <tr>
+            <th>Operaator</th>
+            <th>Nimetus</th>
+            <th>Näide</th>
+            <th>Tulemus</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th>x + y</th>
+            <th>Liitmine</th>
+            <th>".$x." + ".$y."</th>
+            <th>".$liitmine."</th>
+        </tr>
+        </tbody>
+        
+         <tbody>
+        <tr>
+            <th>x - y</th>
+            <th>lahutamine</th>
+            <th>".$x." - ".$y."</th>
+            <th>".$lahutamine."</th>
+        </tr>
+        </tbody>
+        
+        <tbody>
+        <tr>
+            <th>x * y</th>
+            <th>korrutamine</th>
+            <th>".$x." * ".$y."</th>
+            <th>".$korrutamine."</th>
+        </tr>
+        </tbody>
+        
+        <tbody>
+        <tr>
+            <th>x / y</th>
+            <th>jagamine</th>
+            <th>".$x." / ".$y."</th>
+            <th>".$jagamine."</th>
+        </tr>
+        </tbody>
+         
+        <tbody>
+        <tr>
+            <th>x % y</th>
+            <th>Modulus</th>
+            <th>".$x." % ".$y."</th>
+            <th>".$jaak."</th>
+        </tr>
+        </tbody>
+        
+        
+        
+</table>
 ";
+?>
+</body>
+</html>
 
 
-print "liites = ".$liitmine."<br>";
-print "lahutades = ".$lahutamine."<br>";
-print "korrutamine = ".$liitmine."<br>";
+
---
+++ 07.10.2019/Temperatuur.php
@@ -0,0 +1,10 @@
+<?php
+
+$celsius = 5;
+$fahrenheit = (9/5) * $celsius + 32;
+//output
+
+//var_dump($fahrenheit); = float(41)
+
+printf("Celsius: %d&deg;C - fahrenheit: %.2f&deg;F", $celsius, $fahrenheit);
+
